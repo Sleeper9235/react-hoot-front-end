1212e1848d3c59852259a1edd54ab5d7aca96458
Sleeper9235 <kylejyoumans@gmail.com>
2024-07-26 10:21:52 -0500

    built hootDetails component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, createContext, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 import Landing from './components/Landing/Landing';
@@ -6,9 +6,21 @@ import Dashboard from './components/Dashboard/Dashboard';
 import SignupForm from './components/SignupForm/SignupForm'
 import SigninForm from './components/SigninForm/SigninForm';
 import * as authService from '../src/services/authService'
+import HootList from './components/HootList/HootList';
+import * as hootService from './services/hootService'
+import HootDetails from './components/HootDetails/HootDetails';
 
 const App = () => {
   const [user, setUser] = useState(authService.getUser())
+  const [hoots, setHoots] = useState([])
+
+  useEffect(() => {
+    const fetchAllHoots = async () => {
+      const hootsData = await hootService.index()
+      setHoots(hootsData)
+    }
+    if (user) fetchAllHoots()
+  }, [user])
 
   const handleSignout = () => {
     authService.signout()
@@ -20,7 +32,11 @@ const App = () => {
       <NavBar user={user} handleSignout={handleSignout}/>
       <Routes>
         {user ? (
-          <Route path='/' element={<Dashboard user={user} /> } />
+          <>
+            <Route path='/' element={<Dashboard user={user} /> } />
+            <Route path='/hoots' element={<HootList hoots={hoots} /> } />
+            <Route path='/hoots/:hootId' element={<HootDetails /> } />
+          </>
         ) : (
           <Route path='/' element={<Landing /> } />
         )}
